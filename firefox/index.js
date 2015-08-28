2d287c22026d4744612a09347d3ddd781e7dc928
Nathan Cahill <nathan@nathancahill.com>
2015-08-28 13:37:40 -0600

    remove unused files

--- firefox/index.js
+++ firefox/index.js
@@ -3,6 +3,6 @@ var pageMod = require("sdk/page-mod");
  
 pageMod.PageMod({
   include: [/.*github\.com\/.*\/.*/],
-  contentScriptFile: ['./pikaday/pikaday.js', './github-goto-date.js'],
-  contentStyleFile: ['./pikaday/pikaday.css', './github-goto-date.css']
+  contentScriptFile: ['./pikaday/pikaday.js', './github-commit-dates.js'],
+  contentStyleFile: ['./pikaday/pikaday.css']
 });
