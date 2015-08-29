717a687b726bf25963a78025f61bebf86dbf8000
Nathan Cahill <nathan@nathancahill.com>
2015-08-29 11:54:06 -0600

    change project name

--- firefox/index.js
+++ firefox/index.js
@@ -3,6 +3,6 @@ var pageMod = require("sdk/page-mod");
  
 pageMod.PageMod({
   include: [/.*github\.com\/.*\/.*/],
-  contentScriptFile: ['./pikaday/pikaday.js', './github-commit-dates.js'],
+  contentScriptFile: ['./pikaday/pikaday.js', './github-time-travel.js'],
   contentStyleFile: ['./pikaday/pikaday.css']
 });
